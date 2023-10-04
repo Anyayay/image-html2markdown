81b140598e91ec1fa65e07080b8af6a23e916351
Anyayay <anyayay520@gmail.com>
2023-10-04 23:10:40 +0800

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,19 @@
+import re
+import os
+
+folder_path = 'input'
+
+markdown_files = [file for file in os.listdir(folder_path) if file.endswith('.md')]
+
+for file_name in markdown_files:
+    file_path = os.path.join(folder_path, file_name)
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
+        markdown_content = file.read()
+
+    markdown_content = re.sub(r'<img[^>]+src="([^"]+)"[^>]*>', r'![](\1)', markdown_content)
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(markdown_content)
+
+print("Done")
